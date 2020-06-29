f16a753a231292185e60f333ca39149d38663688
tongplw <bangkok.tong@gmail.com>
2020-06-29 09:37:32 +0700

    add zero fill

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import pandas as pd
 
-nav = pd.read_csv('monthly_nav.csv').set_index('Date')
-sel = pd.read_csv('selected_funds.csv').fund_code.to_list()
-nav = nav[[col for col in nav.columns if col in set(sel)]]
-# print(len(set(sel).intersection(set(nav.columns))))
+# nav = pd.read_csv('monthly_nav.csv').set_index('Date')
+# sel = pd.read_csv('selected_funds.csv').fund_code.to_list()
+# nav = nav[[col for col in nav.columns if col in set(sel)]]
+# # print(len(set(sel).intersection(set(nav.columns))))
 
 # for col in nav.columns:
 #     last = nav[col].dropna()
@@ -16,6 +16,12 @@ nav = nav[[col for col in nav.columns if col in set(sel)]]
 
 # nav.to_csv('selected_nav_ffill.csv')
 
-a = set(sel) - set(nav.columns)
-a = list(a)
-pd.DataFrame(a).to_csv('กองที่หายไป.csv')
+# a = set(sel) - set(nav.columns)
+# a = list(a)
+# pd.DataFrame(a).to_csv('กองที่หายไป.csv')
+
+###############################################
+
+df = pd.read_csv('selected_nav_ffill.csv')
+df = df.replace(to_replace=0, method='ffill')
+df.to_csv('selected_nav_ffill_2.csv', index=False)
